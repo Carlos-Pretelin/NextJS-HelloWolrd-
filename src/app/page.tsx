e7f579d04ab9eb6847f8267f67c782527d4af019
Carlos Pretelin <carlospretelin@MacBook-Air-de-Carlos.local>
2024-03-05 15:27:13 -0600

    This is the First Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello World</h1>
+      <p>My name is Carlos Pretelin, im a Frontend Developer!</p>
+    </main>
+  );
+}
