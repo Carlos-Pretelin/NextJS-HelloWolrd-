64b16e1159ad43b098ea70425448667a1f8a7650
Carlos-Pretelin <c_pretelin@hotmail.com>
2024-03-13 14:16:43 -0600

    The article page is working. Deleted the page.tsx in the article folder, it was unnecesary

--- src/app/article/[articleid]/page.tsx
+++ src/app/article/[articleid]/page.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
+import NewsItemProps from "@/types/types";
 
-const articleid = ( {params}: {params: {articleid: string}} ) => {
+const fetchData = async (
+  setArticle: (article: NewsItemProps | null) => void,
+  setError: (error: boolean) => void,
+  id: string
+) => {
+  try {
+    const response = await fetch(
+      `https://itchy-famous-squares-carlosppretelin.replit.app/article/?id=${id}`
+    );
+    const data = await response.json();
+    if (response.status > 204) {
+      console.log("carlos");
+      return;
+    }
+    setArticle(data);
+  } catch (error) {
+    console.error("Error fetching article:", error);
+    setError(true);
+  }
+};
+
+const articleid = ({ params }: { params: { articleid: string } }) => {
+  console.log("Hola")
+
+  const [article, setArticle] = useState<NewsItemProps | null>(null);
+  const [error, setError] = useState(false);
+  const id = params.articleid;
+ console.log({id, params})
+  useEffect(() => {
+    fetchData(setArticle, setError, id as string);
+    console.log("useEffect", id);
+  }, []);
+
+  if (error) {
+    return <h1>Error fetching article </h1>;
+  }
+
+  if (!article) {
+    return;
+  }
+
   return (
-    <div>articleid este es el id: {params.articleid}</div>
-  )
-}
+    <>
+      <h1>This is the article Page</h1>
+      <div className="article">
+        <h2 className="article-title">{article?.headLine}</h2>
+        <p className="article-content">{article?.body}</p>
+        <p className="article-info">Written by Carlos Pretelin on 01/01/2024</p>
+      </div>
+      {error ? <h1>Error fetching article </h1> : null}
+    </>
+  );
+};
 
-export default articleid
+export default articleid;
--- src/components/NewsItem.tsx
+++ src/components/NewsItem.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const NewsItem = ({ item }: { item: NewsItemProps }) => {
   return (
     <div className="newsItem" key={item.id}>
-      <Link key={item.id} href={`/article/${item.id}`} passHref>        
+      <Link href={`/article/${item.id}`} passHref>        
         <h3>{item.headLine}</h3>
         <p>{item.body}</p>
       </Link>
